e74a01a85779575b85b34721a0cb41daeed2f3c7
HyungHoKim00 <rlagudgh1109@gmail.com>
2026-07-26 16:13:26 +0900

    feat: 종목 및 업종별 뉴스 일괄 조회 추가

--- script-service/script_app/repositories.py
+++ script-service/script_app/repositories.py
@@ -6,6 +6,9 @@ from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from script_app.models import (
+    NewsArticle,
+    NewsIndustryMapping,
+    NewsStockMapping,
     StockNewsSummary,
     StockScript,
     UserIndustry,
@@ -77,6 +80,96 @@ class NewsRepository:
 
     def __init__(self, session: Session):
         self.session = session
+
+    def find_by_stock_codes(
+        self,
+        stock_codes: list[str],
+        start_at: datetime,
+        end_at: datetime,
+    ) -> dict[str, list[NewsArticle]]:
+        unique_stock_codes = list(dict.fromkeys(stock_codes))
+        news_by_stock = {
+            stock_code: []
+            for stock_code in unique_stock_codes
+        }
+
+        if not unique_stock_codes:
+            return news_by_stock
+
+        stmt = (
+            select(
+                NewsStockMapping.stock_code,
+                NewsArticle,
+            )
+            .join(
+                NewsArticle,
+                NewsArticle.id == NewsStockMapping.news_id,
+            )
+            .where(
+                NewsStockMapping.stock_code.in_(
+                    unique_stock_codes
+                ),
+                NewsArticle.published_at >= start_at,
+                NewsArticle.published_at < end_at,
+            )
+            .order_by(
+                NewsStockMapping.stock_code,
+                NewsArticle.published_at,
+                NewsArticle.id,
+            )
+        )
+
+        for stock_code, news_article in self.session.execute(stmt):
+            news_by_stock[stock_code].append(news_article)
+
+        return news_by_stock
+
+    def find_by_industry_codes(
+        self,
+        industry_codes: list[str],
+        start_at: datetime,
+        end_at: datetime,
+    ) -> dict[str, list[NewsArticle]]:
+        unique_industry_codes = list(
+            dict.fromkeys(industry_codes)
+        )
+        news_by_industry = {
+            industry_code: []
+            for industry_code in unique_industry_codes
+        }
+
+        if not unique_industry_codes:
+            return news_by_industry
+
+        stmt = (
+            select(
+                NewsIndustryMapping.industry_code,
+                NewsArticle,
+            )
+            .join(
+                NewsArticle,
+                NewsArticle.id == NewsIndustryMapping.news_id,
+            )
+            .where(
+                NewsIndustryMapping.industry_code.in_(
+                    unique_industry_codes
+                ),
+                NewsArticle.published_at >= start_at,
+                NewsArticle.published_at < end_at,
+            )
+            .order_by(
+                NewsIndustryMapping.industry_code,
+                NewsArticle.published_at,
+                NewsArticle.id,
+            )
+        )
+
+        for industry_code, news_article in self.session.execute(
+            stmt
+        ):
+            news_by_industry[industry_code].append(news_article)
+
+        return news_by_industry
     
     def find_news_summaries(
         self,
